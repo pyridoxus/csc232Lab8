c1f3d161d2f8ec33b649766a877aa65c5be08faa
Craig McCulloch <pyridoxus@null.net>
2012-04-17 05:44:26 -0500

    Added GLSL code.

--- source/lab8.h
+++ source/lab8.h
@@ -1,15 +1,11 @@
-/*
- * lab8.h
- *
- *  Created on: Apr 15, 2012
- *      Author: cmcculloch1
- */
-
 #ifndef LAB8_H_
 #define LAB8_H_
 
 using namespace std;
 #include "DataReader.h"
 using namespace DataFileReader;
+#include "GLSL.h"
 
-#endif /* LAB8_H_ */
+#define MAX_STR                   256
+
+#endif
